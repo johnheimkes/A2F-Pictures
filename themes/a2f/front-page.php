abb25a8c5d237e682682a0c1692d472af779780d
John Heimkes IV <bigjohn@papabear.(none)>
2011-10-26 23:32:29 -0500

    finished homepage styling

--- themes/a2f/front-page.php
+++ themes/a2f/front-page.php
@@ -1,19 +1,18 @@
 <?php get_header(); ?>
 <div id="content">
-  
   <div class="slideshow">
     <div class="slideshow-feature">
-      <img src="<?php bloginfo('template_url'); ?>/images/home-booking-summer-2012.jpg" alt="Now Booking Summer 2012." />
-      <!-- <img src="<?php bloginfo('template_url'); ?>/images/home-your-words.jpg" alt="Your works, visualized." />
-      <img src="<?php bloginfo('template_url'); ?>/images/home-reach-customers.jpg" alt="Reach your customers." />
-      <img src="<?php bloginfo('template_url'); ?>/images/home-your-project.jpg" alt="Your project, your way." /> -->
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-booking-summer-2012.jpg" alt="Now Booking Summer 2012." /></a>
+      <!-- <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-your-words.jpg" alt="Your works, visualized." /></a>
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-reach-customers.jpg" alt="Reach your customers." /></a>
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-your-project.jpg" alt="Your project, your way." /></a> -->
     </div>
     
     <div class="slideshow-nav">
-      <img src="<?php bloginfo('template_url'); ?>/images/home-booking-summer-2012-small.jpg" alt="Now Booking Summer 2012." />
-      <img src="<?php bloginfo('template_url'); ?>/images/home-your-words-small.jpg" alt="Your works, visualized." />
-      <img src="<?php bloginfo('template_url'); ?>/images/home-reach-customers-small.jpg" alt="Reach your customers." />
-      <img src="<?php bloginfo('template_url'); ?>/images/home-your-project-small.jpg" alt="Your project, your way." />
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-booking-summer-2012-small.jpg" alt="Now Booking Summer 2012." /></a>
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-your-words-small.jpg" alt="Your works, visualized." /></a>
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-reach-customers-small.jpg" alt="Reach your customers." /></a>
+      <a href="#"><img src="<?php bloginfo('template_url'); ?>/images/home-your-project-small.jpg" alt="Your project, your way." /></a>
     </div>
   </div>
 </div>
--- themes/a2f/header.php
+++ themes/a2f/header.php
@@ -2,7 +2,7 @@
 <html <?php language_attributes(); ?>>
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
-    <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
+    <title><?php bloginfo( 'name' ); ?> <?php wp_title(); ?></title>
     <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
     <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
     <?php wp_head(); ?>
@@ -12,12 +12,12 @@
   <body <?php body_class(); ?>>
     <div id="wrapper">
       <div id="header">
-        <h1 class="header-logo">A2F Pictures</h1>
+        <h1><a href="<?php bloginfo('url') ?>" class="header-logo">A2F Pictures</a></h1>
         <ul class="nav">
           <li class="nav-item"><a href="<?php bloginfo('url') ?>" class="nav-link <?php if ( is_home() ) { echo 'active-page'; } ?>">Home</a></li>
           <li class="nav-item"><a href="<?php bloginfo('url') ?>/portfolio" class="nav-link <?php if ( is_page(array('portfolio', 'wedding', 'commercial', 'music-video', 'speciality')) ) { echo 'active-page'; } ?>">Portfolio</a></li>
           <li class="nav-item"><a href="<?php bloginfo('url') ?>/quote" class="nav-link <?php if ( is_page('quote') ) { echo 'active-page'; } ?>">Quote</a></li>
-          <li class="nav-item"><a href="<?php bloginfo('url') ?>" class="nav-link <?php if ( is_page('contact') ) { echo 'active-page'; } ?>">Contact</a></li>
+          <li class="nav-item"><a href="<?php bloginfo('url') ?>/contact" class="nav-link <?php if ( is_page('contact') ) { echo 'active-page'; } ?>">Contact</a></li>
         </ul>
         
         <!-- <div id="nav">

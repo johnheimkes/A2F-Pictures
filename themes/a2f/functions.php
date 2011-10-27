8644900224b43ac914b3c7a2fee9d42688eba01e
John Heimkes IV <bigjohn@papabear.(none)>
2011-10-26 19:35:01 -0500

    exported the rest of the images

--- themes/a2f/functions.php
+++ themes/a2f/functions.php
@@ -5,3 +5,11 @@
 //	return 20;
 //}
 //add_filter('excerpt_length', 'new_excerpt_length');
+
+function register_my_menus() {
+  register_nav_menus(
+    array('header-menu' => __( 'Header Menu' ) )
+  );
+}
+
+add_action( 'init', 'register_my_menus' );
--- themes/a2f/header.php
+++ themes/a2f/header.php
@@ -3,17 +3,17 @@
   <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8" />
     <title><?php wp_title(); ?> <?php bloginfo( 'name' ); ?></title>
-    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>" type="text/css" media="screen" />
-    <link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/images/favicon.ico" />
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" media="screen" />
+    <link rel="shortcut icon" href="<?php bloginfo('template_url'); ?>/images/favicon.ico" />
     <?php wp_head(); ?>
-    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.1/jquery.min.js"></script
-    <script type="text/javascript" src="<?php bloginfo('template_url') ?>/js/app.js"></script>
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script
+    <script type="text/javascript" src="<?php bloginfo('template_url'); ?>/js/app.js"></script>
   </head>
   <body <?php body_class(); ?>>
     <div id="wrapper">
       <div id="header">
-        
+        <h1 class="header-logo">A2F Pictures</h1>
         <div id="nav">
-          
+          <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
         </div>
       </div>

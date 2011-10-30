9e2bfb7be6c135852ea83bf7ec1b4a17ac33c4d3
John Heimkes IV <bigjohn@papabear.(none)>
2011-10-30 17:38:30 -0500

    added real links to footer links

--- themes/a2f/footer.php
+++ themes/a2f/footer.php
@@ -2,9 +2,9 @@
         <p>Copyright &copy; 2011 &ndash; A2F Pictures LLC</p>
         <p><a href="mailto:&#105;&#110;&#102;&#111;&#064;&#097;&#050;&#102;&#112;&#105;&#099;&#116;&#117;&#114;&#101;&#115;&#046;&#099;&#111;&#109;">info@a2fpictures.com</a></p>
         <ul class="foot-nav">
-          <li class="foot-nav-link"><a href="#" class="nav-link" target="_blank">Vimeo</a></li>
-          <li class="foot-nav-link"><a href="#" class="nav-link" target="_blank">Twitter</a></li>
-          <li class="foot-nav-link"><a href="#" class="nav-link" target="_blank">Facebook</a></li>
+          <li class="foot-nav-link"><a href="http://vimeo.com/channels/a2fpictures" class="nav-link" target="_blank">Vimeo</a></li>
+          <li class="foot-nav-link"><a href="http://twitter.com/#!/a2fpictures" class="nav-link" target="_blank">Twitter</a></li>
+          <li class="foot-nav-link"><a href="http://facebook.com/A2fPictures" class="nav-link" target="_blank">Facebook</a></li>
         </ul>
       </div>
     </div>

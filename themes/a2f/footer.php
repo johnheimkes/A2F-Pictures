811296731c7c3a93c29f1f93815420355ac47a91
John Heimkes IV <bigjohn@papabear.(none)>
2011-10-31 20:37:10 -0500

    well fuck my ass and call me a bitch... revisions at the 11th fucking hour

--- themes/a2f/footer.php
+++ themes/a2f/footer.php
@@ -4,8 +4,7 @@
         <p><a href="mailto:&#105;&#110;&#102;&#111;&#064;&#097;&#050;&#102;&#112;&#105;&#099;&#116;&#117;&#114;&#101;&#115;&#046;&#099;&#111;&#109;">info@a2fpictures.com</a></p>
         
         <address>
-          653 Galtier Street, Ste. 204 <br />
-          Saint Paul, MN 55103
+          653 Galtier Street, Ste. 204, Saint Paul, MN 55103
         </address>
         
         <ul class="foot-nav">

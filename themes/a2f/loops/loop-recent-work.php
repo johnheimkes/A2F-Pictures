64bd9d644e2b747b38b3dfe9c6fa934ec8adab2b
John Heimkes IV <john@johnheimkes.com>
2012-04-09 23:00:38 -0500

    took content div out of recent-works loop file but still need to add the sidebar to that page and single recent works

--- themes/a2f/loops/loop-recent-work.php
+++ themes/a2f/loops/loop-recent-work.php
@@ -3,14 +3,12 @@
 
 while ( $my_recent_works_query->have_posts() ) : $my_recent_works_query->the_post(); ?>
 
-<div id="content">
     <div class="post recent-work">
-         <h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
-        <div class="entrytext">
-          <?php the_content(); ?>
-          <a href="<?php the_permalink(); ?>">Read more</a>
-        </div>
-    </div>
-</div>
+		<h2 id="post-<?php the_ID(); ?>"><a href="<?php the_permalink(); ?>"><?php the_title();?></a></h2>
+		<div class="entrytext">
+			<?php the_content(); ?>
+			<a href="<?php the_permalink(); ?>">Read more</a>
+		</div>
+	</div>
 
 <?php endwhile; ?>
--- themes/a2f/sidebar.php
+++ themes/a2f/sidebar.php
@@ -1,5 +1,5 @@
 <div id="sidebar">
-	<?php if (is_page('portfolio') || is_post_type_archive('recent-works')) :
+	<?php if (is_page('portfolio') || is_page(85)) :
 		wp_nav_menu( array('menu' => 'Portfolio-Nav' ));
 	 endif; ?>
 </div>

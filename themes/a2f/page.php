d08d5aae75b6097b1b6bd4f9ff2c4c9517d21268
John Heimkes IV <john@johnheimkes.com>
2012-04-10 23:38:40 -0500

    fuck all. client is being a douche.

--- themes/a2f/page.php
+++ themes/a2f/page.php
@@ -1,10 +1,19 @@
 <?php get_header(); ?>
 <div id="content">
-	<?php get_sidebar(); ?>
-	<div class="main-column">
-    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-		<?php the_content(); ?>
-    <?php endwhile; endif; ?>
-	</div>
+	<?php if (is_page('recent-work')) : ?>
+		<?php get_sidebar(); ?>
+		
+		<div class="main-column">
+	    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+	    <?php endwhile; endif; ?>
+		</div>
+	<?php else : ?>
+		
+	    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			<?php the_content(); ?>
+	    <?php endwhile; endif; ?>
+		
+	<?php endif; ?>
 </div>
 <?php get_footer(); ?>

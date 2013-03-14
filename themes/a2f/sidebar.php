2ea99e7c2a5c9c25e70a64b9e8dd5633ac562ef5
John Heimkes IV <john@johnheimkes.com>
2013-03-13 21:23:21 -0500

    added WP 3.5.1 as a submodule

--- themes/a2f/sidebar.php
+++
@@ -1,5 +0,0 @@
-<div id="sidebar">
-	<?php if (is_page(array('portfolio', 'recent-work')) || $post->post_parent=="recent-work" || $post->post_parent) : ?>
-		<?php wp_nav_menu(array('menu' => 'Portfolio-Nav' )); ?>
-	<?php endif; ?>
-</div>

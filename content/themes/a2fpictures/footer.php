837ca6205a443373e4c939b2a654f8e96d9d5c6c
John Heimkes IV <john@johnheimkes.com>
2013-03-25 23:16:27 -0500

    finished footer styling and added that stupid fucking bar at the bottom of the window that will break unless i put some fucking JS in there

--- content/themes/a2fpictures/footer.php
+++ content/themes/a2fpictures/footer.php
@@ -15,10 +15,37 @@
         <footer class="mast-footer">
             <div class="footer">
                 
+                <div class"copyright">
+                    Copyright &copy; <?php echo date('Y'); ?> &ndash; A2F Pictures LLC
+                </div>
+                
+                <div class="contact">
+                    <a href="mailto:&#105;&#110;&#102;&#111;&#064;&#097;&#050;&#102;&#112;&#105;&#099;&#116;&#117;&#114;&#101;&#115;&#046;&#099;&#111;&#109;" class="mailto">info@a2fpictures.com</a>
+                    <address>
+                        653 Galtier Street, Ste. 204, Saint Paul, MN, 55103
+                    </address>
+                </div>
+                
+                <ul class="social-media-list">
+                    <li class="social-media-item">
+                        <a href="http://www.youtube.com/user/A2fPicturesLLC" target="_blank">YouTube</a>
+                    </li>
+                    <li class="social-media-item">
+                        <a href="http://vimeo.com/channels/a2fpictures" target="_blank">Vimeo</a>
+                    </li>
+                    <li class="social-media-item">
+                        <a href="http://twitter.com/#!/a2fpicture" target="_blank">Twitter</a>
+                    </li>
+                    <li class="social-media-item">
+                        <a href="http://facebook.com/A2fPictures" target="_blank">Facebook</a>
+                    </li>
+                </ul>
+                
             </div>
         </footer>
         
     </div>
+    <div class="bottom-bg"></div>
 <?php wp_footer(); ?>
 </body>
 </html>

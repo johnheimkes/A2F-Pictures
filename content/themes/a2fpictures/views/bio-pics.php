8a860152a5d88a01be63e6d329adbbd3fecbe6d4
John Heimkes IV <john@markupisart.com>
2013-03-27 22:24:31 -0500

    added opacity change to inactive bio pics

--- content/themes/a2fpictures/views/bio-pics.php
+++ content/themes/a2fpictures/views/bio-pics.php
@@ -1,8 +1,8 @@
-<div class="grid-col-12">
+<div class="grid-col-12 page-head">
     <div class="grid-col-4 grid-col director-wrapper">
         <div class="director-image">
             <a href="<?php echo site_url('directors/trent-hilborn'); ?>">
-                <img src="<?php echo A2F_THEME_PATH_URL; ?>/assets/images/bios/trent-bw.jpg" alt="Trent Hilborn" />
+                <img src="<?php echo A2F_THEME_PATH_URL; ?>assets/images/bios/trent-color.jpg" alt="Trent Hilborn" <?php if ( is_page( 'trent-hilborn' ) ) { echo 'class="active-bio"'; } elseif ( is_page( array('mark-mazur', 'james-rautmann') ) ) { echo 'class="inactive-bio"'; } ?> />
             </a>
         </div>
     </div>
@@ -10,7 +10,7 @@
     <div class="grid-col-4 grid-col director-wrapper">
         <div class="director-image">
             <a href="<?php echo site_url('directors/mark-mazur'); ?>">
-                <img src="<?php echo A2F_THEME_PATH_URL; ?>/assets/images/bios/mark-bw.jpg" alt="Mark Mazur" />
+                <img src="<?php echo A2F_THEME_PATH_URL; ?>assets/images/bios/mark-color.jpg" alt="Mark Mazur" <?php if ( is_page( 'mark-mazur' ) ) { echo 'class="active-bio"'; } elseif ( is_page( array('trent-hilborn', 'james-rautmann') ) ) { echo 'class="inactive-bio"'; } ?> />
             </a>
         </div>
     </div>
@@ -18,7 +18,7 @@
     <div class="grid-col-4 grid-col director-wrapper">
         <div class="director-image ">
             <a href="<?php echo site_url('directors/james-rautmann'); ?>">
-                <img src="<?php echo A2F_THEME_PATH_URL; ?>/assets/images/bios/james-bw.jpg" alt="James Rautmann" />
+                <img src="<?php echo A2F_THEME_PATH_URL; ?>assets/images/bios/james-color.jpg" alt="James Rautmann" <?php if ( is_page( 'james-rautmann' ) ) { echo 'class="active-bio"'; } elseif ( is_page( array('trent-hilborn', 'mark-mazur') ) ) { echo 'class="inactive-bio"'; } ?> />
             </a>
         </div>
     </div>

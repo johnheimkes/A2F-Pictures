3172d51980022931f488ea30e5072da0597ac99b
John Heimkes IV <john@johnheimkes.com>
2014-06-18 21:03:30 -0500

    who the fuck knows

--- content/themes/a2fpictures/page-director-single.php
+++ content/themes/a2fpictures/page-director-single.php
@@ -17,29 +17,30 @@ get_header(); ?>
 
 <?php get_sidebar(); ?>
 
-<a name="scroll"></a>
-
 <div class="grid-col grid-col-9">
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <div class="grid-col-12">
-            <div class="grid-col-6 grid-col">
-                <div class="director-header">
-                    <h2 class="title fleft"><?php the_title(); ?></h2>
-                </div>
-                
-                <h3 class="client-name oblique"><?php the_field('job_title'); ?></h3>
-                <?php echo get_the_author_meta('description'); ?>
-            </div>
             
-            <div class="grid-col-6 grid-col">
+            <div class="director-bts">
                 <img src="<?php the_field( 'content_image' ); ?>" alt="<?php the_title(); ?>" class="block" />
             </div>
+            
+            <div class="director-info">
+                <div class="grid-col-6 grid-col">
+                    <div class="director-header">
+                        <h2 class="title fleft"><?php the_title(); ?></h2>
+                        <a href="mailto:elizabeth@a2fpictures.com?Subject=Reel%20Request:%20<?php the_title(); ?>" class="btn-request-reel fright">Request Reel</a>
+                    </div>
+                
+                    <h3 class="client-name oblique"><?php the_field('job_title'); ?></h3>
+                    <?php echo get_the_author_meta('description'); ?>
+                </div>
+            
         
             <?php if ( get_the_author_meta('display_name') == 'Mark Mazur') { ?>
-                <div class="related-module grid-col-8 grid-col fright">
+                <div class="related-module grid-col-6 grid-col fright">
                     <div class="module-header">
                         <h3 class="title fleft">Projects Directed By <?php the_author(); ?></h3>
-                        <a href="mailto:elizabeth@a2fpictures.com?Subject=Reel%20Request:%20<?php the_title(); ?>" class="btn-request-reel fright">Request Reel</a>
                     </div>
                 
                     <?php
@@ -55,7 +56,7 @@ get_header(); ?>
                                         <?php the_post_thumbnail('more-by-thumbnail'); ?>
                                     </div>
                                     <div class="post-info">
-                                        <h4 class="related-post-title"><?php the_title(); ?> &rarr;</h4>
+                                        <h4 class="related-post-title"><?php the_title(); ?></h4>
                                     </div>
                                 </a>
                             </li>
@@ -66,10 +67,9 @@ get_header(); ?>
             <?php } ?>
         
             <?php if ( get_the_author_meta('display_name') == 'James Rautmann') { ?>
-                <div class="related-module grid-col-8 grid-col fright">
+                <div class="related-module grid-col-6 grid-col fright">
                     <div class="module-header">
                         <h3 class="title fleft">Projects Directed By <?php the_author(); ?></h3>
-                        <a href="mailto:elizabeth@a2fpictures.com?Subject=Reel%20Request:%20<?php the_title(); ?>" class="btn-request-reel fright">Request Reel</a>
                     </div>
                 
                     <?php
@@ -85,7 +85,7 @@ get_header(); ?>
                                         <?php the_post_thumbnail('more-by-thumbnail'); ?>
                                     </div>
                                     <div class="post-info">
-                                        <h4 class="related-post-title"><?php the_title(); ?> &rarr;</h4>
+                                        <h4 class="related-post-title"><?php the_title(); ?></h4>
                                     </div>
                                 </a>
                             </li>
@@ -96,10 +96,9 @@ get_header(); ?>
             <?php } ?>
         
             <?php if ( get_the_author_meta('display_name') == 'Trent Hilborn') { ?>
-                <div class="related-module grid-col-8 grid-col fright">
+                <div class="related-module grid-col-6 grid-col">
                     <div class="module-header">
                         <h3 class="title fleft">Projects Directed By <?php the_author(); ?></h3>
-                        <a href="mailto:elizabeth@a2fpictures.com?Subject=Reel%20Request:%20<?php the_title(); ?>" class="btn-request-reel fright">Request Reel</a>
                     </div>
                 
                     <?php
@@ -115,7 +114,7 @@ get_header(); ?>
                                         <?php the_post_thumbnail('more-by-thumbnail'); ?>
                                     </div>
                                     <div class="post-info">
-                                        <h4 class="related-post-title"><?php the_title(); ?> &rarr;</h4>
+                                        <h4 class="related-post-title"><?php the_title(); ?></h4>
                                     </div>
                                 </a>
                             </li>
@@ -129,7 +128,6 @@ get_header(); ?>
                 <div class="related-module grid-col-8 grid-col">
                     <div class="module-header">
                         <h3 class="title fleft">Projects Directed By <?php the_author(); ?></h3>
-                        <a href="mailto:elizabeth@a2fpictures.com?Subject=Reel%20Request:%20<?php the_title(); ?>" class="btn-request-reel fright">Request Reel</a>
                     </div>
                 
                     <?php
@@ -154,6 +152,8 @@ get_header(); ?>
                     <?php wp_reset_postdata(); endif; ?>
                 </div>
             <?php } ?>
+            
+            </div>
             
         </div>
     <?php endwhile; endif; ?>

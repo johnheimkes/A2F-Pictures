3e778b342e2714d4652f3725aba076918d777a00
John Heimkes IV <john@markupisart.com>
2014-10-03 10:45:50 -0400

    added simple page template and merging uploads

--- content/themes/a2fpictures/page.php
+++ content/themes/a2fpictures/page.php
@@ -11,4 +11,13 @@
 
 get_header(); ?>
 
+<div class="grid-col-12 page-content">
+    
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    	<h2 class="title"><?php the_title(); ?></h2>
+    	<?php the_content(); ?>
+	<?php endwhile; endif; ?>
+    
+</div>
+
 <?php get_footer(); ?>

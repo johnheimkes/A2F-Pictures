fb9012d7109efc9e2a6b78e39297b37e3edb12c3
John Heimkes IV <john@markupisart.com>
2014-10-07 11:32:08 -0400

    took title out of page.php

--- content/themes/a2fpictures/page.php
+++ content/themes/a2fpictures/page.php
@@ -14,7 +14,6 @@ get_header(); ?>
 <div class="grid-col-12 page-content">
     
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    	<h2 class="title"><?php the_title(); ?></h2>
     	<?php the_content(); ?>
 	<?php endwhile; endif; ?>
     
